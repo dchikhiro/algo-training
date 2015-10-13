e27939e1619eb672f30475d310230d3b6797078c
Dmitry Chikhiro <dchikhiro@gmail.com>
2015-10-13 18:07:04 +0300

    Initial commit. Non compilable version.

---
+++ merge.py
@@ -0,0 +1,11 @@
+
+class array_i:
+	data = []
+	def __init__(self, n):
+		for x in xrange(1, n)
+			data.append(random.uniform(-100,100))
+	def randomize():
+		for i,D in data
+			data[i] = random.uniform(-100,100)
+	def merge_sort():
+		
